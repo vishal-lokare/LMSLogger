17003866b64d2b476de45950aa1d8a4eb43be58f
vishal-lokare <31512838+vishal-lokare@users.noreply.github.com>
2021-07-07 10:44:02 +0530

    Update logger.js

--- logger.js
+++ logger.js
@@ -7,7 +7,8 @@ $(document).ready( ()=> {
 
 	if(url == 'https://lms.iiitkottayam.ac.in/login/index.php') {
 		//replace your ID below
-		document.getElementById('username').value = "2020BCS0120";		document.getElementById('username').defaultValue = "replace_ID";
+		document.getElementById('username').value = "replace_ID";
+		
 		//replace your password below
 		document.getElementById('password').value = "replace_password";
 		document.getElementsByClassName('btn btn-primary btn-block mt-3')[0].click();
