0d2163e434d346babd89d6fa21eaa4b23632088f
Proximity Destructor <harshessi@gmail.com>
2023-11-24 11:14:23 +0530

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,24 +1,21 @@
 $(document).ready(function () {
 	var url = window.location.href
 	console.log(url);
-
+	let id="Enter_ID"
+	let pass="Enter_Pass"
 	if( url == 'https://lms.iiitkottayam.ac.in/'){
 		document.getElementsByClassName('btn-login').click();
 		document.getElementsByClassName('btn btn-login-top dropdown-toggle').click();
-		document.getElementById('username').value="ID_HERE"; //Enter your ID here
-		document.getElementById('password').value="PASS_HERE"; //Enter your password here
+		document.getElementById('username').value=id; //Enter your ID here
+		document.getElementById('password').value=pass; //Enter your password here
 		document.getElementsByClassName('btn btn-primary').click();}
 
 	if(url == 'https://lms.iiitkottayam.ac.in/login/index.php' || url == 'https://lms.iiitkottayam.ac.in/login/index.php/#') {
 		//replace your ID below
-		document.getElementById('username').value = "replace_ID";
+		document.getElementById('username').value = id;
 		
 		//replace your password below
-		document.getElementById('password').value = "replace_password";
-<<<<<<< HEAD
-		document.getElementsById('loginbtn').click();
-=======
+		document.getElementById('password').value = pass;
 		document.getElementById('loginbtn').click();
->>>>>>> 80401efd5080d4cc0699b63ed5346015edab1cf1
 	}
 })
