fc78933ce1dcacc06a1c4abc209b775b7c5c2b8f
Proximity Destructor <harshessi@gmail.com>
2023-11-24 09:54:57 +0530

    Update logger.js

--- logger.js
+++ logger.js
@@ -2,14 +2,12 @@ $(document).ready(function () {
 	var url = window.location.href
 	console.log(url);
 
-	if( url == 'https://lms.iiitkottayam.ac.in/')
+	if( url == 'https://lms.iiitkottayam.ac.in/'){
 		document.getElementsByClassName('btn-login')[0].click();
-	if(url == 'https://lms.iiitkottayam.ac.in'){
-		document.getElementByClassName('btn btn-login-top dropdown-toggle')[0].click();
+		document.getElementsByClassName('btn btn-login-top dropdown-toggle')[0].click();
 		document.getElementById('username').value="ID_HERE"; //Enter your ID here
 		document.getElementById('password').value="PASS_HERE"; //Enter your password here
-		document.getElementByClass('btn btn-primary')[0].click();
-	}
+		document.getElementsByClassName('btn btn-primary')[0].click();}
 
 	if(url == 'https://lms.iiitkottayam.ac.in/login/index.php' || url == 'https://lms.iiitkottayam.ac.in/login/index.php/#') {
 		//replace your ID below
